cfbef7826db9c0e789c15f9425266f2b26d6b5fd
martin bucarey <martinebucarey@gmail.com>
2019-11-12 13:59:49 -0300

    feat(monitor-app): se agrega logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { environment } from './../environments/environment';
 import { Server } from '@andes/shared';
+import { Plex } from '@andes/plex';
+
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,17 @@ import { Server } from '@andes/shared';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public server: Server) {
+
+  private menuList = [];
+
+  constructor(public server: Server, public plex: Plex) {
     server.setBaseURL(environment.API);
+    this.crearMenu();
+  }
+
+  public crearMenu() {
+    this.menuList.push({ label: 'Webhooks', icon: 'hook', route: '/webhook' });
+    this.menuList.push({ label: 'Cerrar Sesión', icon: 'logout', route: '/login' });
+    this.plex.updateMenu(this.menuList);
   }
 }
--- src/app/webhook/services/webhook.service.ts
+++ src/app/webhook/services/webhook.service.ts
@@ -5,7 +5,7 @@ import { Server } from '@andes/shared';
 
 @Injectable()
 export class WebHookService {
-    private webHookUrl = '/modules/webhook'; 
+    private webHookUrl = '/modules/webhook';
 
     constructor(private server: Server) { }
 
@@ -17,20 +17,20 @@ export class WebHookService {
         return this.server.post(this.webHookUrl + '/webhooks/', webHook);
     }
 
-    patch(id: String, cambios: any, options: any = {}): Observable<IWebhook> {
-        return this.server.patch(this.webHookUrl +'/webhooks/'+`${id}`, cambios);
+    patch(id, cambios: any, options: any = {}): Observable<IWebhook> {
+        return this.server.patch(this.webHookUrl + '/webhooks/' + `${id}`, cambios);
     }
 
     delete(webHook: IWebhook): Observable<any> {
-        return this.server.delete(this.webHookUrl+'/webhooks/'+`${webHook.id}`);
+        return this.server.delete(this.webHookUrl + '/webhooks/' + `${webHook.id}`);
     }
-    
-    get(nombre: String): Observable<IWebhook[]> {
-        return this.server.get(this.webHookUrl +'/webhooks/'+`${nombre}`);
+
+    get(nombre): Observable<IWebhook[]> {
+        return this.server.get(this.webHookUrl + '/webhooks/' + `${nombre}`);
     }
 
 
-    
+
 
 
 }
