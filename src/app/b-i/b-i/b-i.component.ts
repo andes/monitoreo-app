870af8fb704dd29a2451b67110bd3167be3ee188
celeste <MCele>
2020-01-07 11:34:17 -0300

    biqueries(feat): agrega verificación de argumentos

--- src/app/b-i/b-i/b-i.component.ts
+++ src/app/b-i/b-i/b-i.component.ts
@@ -17,15 +17,16 @@ export class BIComponent implements OnInit {
   private listaArgumentos = [];
   private selectConsulta: IFiltroBi;
   private seleccionado: boolean;
-  constructor(public plex: Plex, private biService: BIService) {
 
+  constructor(public plex: Plex, private biService: BIService) {
+    // this.argumetosCargados = false;
     this.selectConsulta = null;
     this.biService.getAllQuerys().subscribe(
       resultado => {
         this.listaFiltro = resultado;
       },
       (err) => {
-        // console.log("error desde get: ", err);
+        this.plex.info('warning', 'En este momento no podemos procesar su pedido, intentelo más tarde', 'Error al obtener Queries');
       }
     );
   }
@@ -34,20 +35,51 @@ export class BIComponent implements OnInit {
   }
 
   elegirConsulta() {
-    // Traemos de la BD los argumentos para esa consulta (armamos listaArgumentos de acuerdo a lo elegido)
     if (this.selectConsulta) {
       this.listaArgumentos = this.selectConsulta.argumentos;
-
     }
   }
 
-  mostrar(event) {
-    console.log(event);
+  mostrar() {
+    let argumetosCarg = true;
+    if ((this.listaArgumentos) && (this.listaArgumentos.length > 0)) {
+      this.listaArgumentos.forEach(arg => {
+        argumetosCarg = argumetosCarg && (arg.valor !== undefined) && (arg.valor !== null);
+      });
+    }
+    return argumetosCarg;
   }
-  descargarCSV(event) {
+  descargarCSV() {
     if (this.selectConsulta) {
       let nombre = this.selectConsulta;
-      this.biService.descargar(this.selectConsulta);
+      //this.argumetosCargados = true;
+
+      if (this.verificarFechas()) {
+        this.biService.descargar(this.selectConsulta);
+      }
     }
+  }
+  verificarFechas() {
+    // revisamos el contenido de los argumentos,
+    // si son intervalos de fechas comparamos orden fechaInicio < fechaFin
+    let fechasOk = true;
+    let fechaI: Date;
+    let fechaF: Date;
+    if ((this.listaArgumentos) && (this.listaArgumentos.length > 0)) {
+      this.listaArgumentos.forEach(arg => {
+        if (arg.tipo === 'date') {
+          switch (arg.nombre) {
+            case 'fechaInicio': if (!arg.valor) { fechasOk = false; } else { fechaI = new Date(arg.valor); } break;
+            case 'fechaFin': if (!arg.valor) { fechasOk = false; } else { fechaF = new Date(arg.valor); } break;
+            default: fechasOk = (arg.valor);
+          }
+        }
+      });
+    }
+    if (fechasOk && fechaF && fechaI && (fechaF < fechaI)) {
+      this.plex.info('danger', 'La fecha final no puede ser menor a la fecha inicial', 'Error en Fechas');
+      fechasOk = false;
+    }
+    return fechasOk;
   }
 }
