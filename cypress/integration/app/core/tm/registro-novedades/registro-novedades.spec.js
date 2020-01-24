de8678f1021cf54129ca7e917a2df70c6ee4480d
celeste <MCele>
2020-01-24 15:08:02 -0300

    test(novedades):casos de test para RegistroNovedad

--- cypress/integration/app/core/tm/registro-novedades/registro-novedades.spec.js
+++ cypress/integration/app/core/tm/registro-novedades/registro-novedades.spec.js
@@ -14,10 +14,10 @@ context('Registro novedades', () => {
         cy.route('GET', '**api/modules/registro-novedades/novedades**').as('getNovedades');
         cy.goto('/registro-novedades', token);
         cy.route('POST', '**/api/modules/registro-novedades/novedades').as('postNovedades');
+        cy.route('PATCH', '**/api/modules/registro-novedades/novedades/**').as('patchNovedades');
     });
 
-
-    it.skip('Cargar lista de novedades al inicio de la pantalla', () => {
+    it('Cargar lista de novedades al inicio de la pantalla', () => {
         cy.wait('@getNovedades').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
         });
@@ -45,11 +45,28 @@ context('Registro novedades', () => {
             expect(xhr.status).to.be.eq(200);
         });
 
+<<<<<<< HEAD
         //verificamos que la lista se carguenuevamente
+=======
+        //verificamos que la lista se cargue nuevamente
+>>>>>>> test(novedades):casos de test para RegistroNovedad
         cy.wait('@getNovedades').then((xhr) => {
             expect(xhr.status).to.be.eq(200);
         });
         cy.get('table tbody td').contains(titulo).click();
     });
 
+<<<<<<< HEAD
+=======
+    it('Se busca la primer novedad cargada y se activa', () => {
+        cy.get('table tbody td').find('span').should('have.class', 'badge badge-success badge-md').first().click();
+        cy.plexBool('label="Activa"', true);
+        cy.plexButton('Guardar').click();
+        cy.wait('@patchNovedades').then((xhr) => { // verificamos que guarde el editar del registro
+            expect(xhr.status).to.be.eq(200);
+        });
+        cy.get('table tbody td').find('span').should('have.class', 'badge badge-success badge-md').first().contains(' Activa ');
+    });
+
+>>>>>>> test(novedades):casos de test para RegistroNovedad
 });
