f85d79269a0a43c5c5002b01e560115b62f61a9e
Natalia Huenchuman <natalia.huenchuman@gmail.com>
2020-03-18 08:48:12 -0300

    refactor(app): agrega RoutingNavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AuthModule, Auth } from '@andes/auth';
-import { RoutingGuard } from './login/routing-guard';
+import { RoutingGuard, RoutingNavBar } from './login/routing-guard';
 import { WebHookComponent } from './webhook/components/webhook.component';
 import { WebHookService } from './webhook/services/webhook.service';
 import { ConceptosTurneablesComponent } from './conceptos-turneables/components/conceptos-turneables.component';
@@ -50,6 +50,7 @@ import { SendMessageCacheService } from './monitor-activaciones/services/sendMes
     Auth,
     HttpClient,
     RoutingGuard,
+    RoutingNavBar,
     WebHookService,
     ConceptoTruneableService,
     SnomedService,
