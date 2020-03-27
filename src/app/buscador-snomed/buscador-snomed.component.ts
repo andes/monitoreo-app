db31f3ffdcc3a4591788024a026c72abb04ef3c3
Juliana <juli.otero01@gmail.com>
2020-03-27 15:27:10 -0300

    feat(monitoreo): buscador de conceptos snomed

---
+++ src/app/buscador-snomed/buscador-snomed.component.ts
@@ -0,0 +1,37 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Plex } from '@andes/plex';
+import { ISnomedConcept } from '../shared/ISnomedConcept';
+import { SnomedService } from '../shared/snomed.service';
+
+@Component({
+    selector: 'app-buscador-snomed',
+    templateUrl: './buscador-snomed.component.html'
+})
+export class BuscadorSnomedComponent implements OnInit {
+    searchTerm: string;
+    busqueda: string;
+    conceptos: ISnomedConcept[];
+
+    constructor(
+        private plex: Plex,
+        private snomedService: SnomedService
+    ) { }
+
+    ngOnInit() {
+    }
+
+    buscarConcepto() {
+        if (this.searchTerm.match(/<<\s{1,}/)) {
+            this.searchTerm = '';
+            return;
+        }
+        const query = {
+            search: this.searchTerm
+        };
+        this.snomedService.get(query).subscribe((resultado: ISnomedConcept[]) => {
+            this.conceptos = resultado;
+        });
+    }
+
+}
