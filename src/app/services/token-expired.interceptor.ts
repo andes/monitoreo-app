3ea2e38fd58217a7fab8ffa5cb61f16d82fe97ee
ma7payne <matich04@gmail.com>
2023-11-28 14:17:35 -0300

    feat(MONIT): agrega control para cierre de sesion al vencer token (#59)

---
+++ src/app/services/token-expired.interceptor.ts
@@ -0,0 +1,34 @@
+import { Auth } from '@andes/auth';
+import { Plex } from '@andes/plex';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable()
+export class TokenExpiredInterceptor implements HttpInterceptor {
+    constructor(
+        private plex: Plex,
+        private router: Router,
+        private auth: Auth
+    ) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError((err) => {
+                if (err.status === 401 && err.error === 'Unauthorized') {
+                    this.plex.info('danger', '', 'La sesión ha finalizado!').then((a) => {
+                        this.auth.logout();
+                        this.router.navigate(['login']).then(() => {
+                            window.location.reload();
+                        });
+                    });
+                    return of(null);
+                }
+                return throwError(err);
+            })
+        );
+    }
+}
