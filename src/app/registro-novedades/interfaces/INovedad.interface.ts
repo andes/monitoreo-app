361ca2ac51db5d4ce6174617794f979690a34739
ma7payne <matich04@gmail.com>
2024-02-29 15:21:14 -0300

    feat(MONIT): actualizacion de ABM en novedades (#62)

--- src/app/registro-novedades/interfaces/INovedad.interface.ts
+++ src/app/registro-novedades/interfaces/INovedad.interface.ts
@@ -3,6 +3,7 @@ import { IModulo } from 'src/app/modulos/interfaces/IModulo.interface';
 export interface INovedad {
     _id?: string;
     titulo: string;
+    palabra: string;
     fecha: Date;
     descripcion: string;
     modulo: IModulo;
