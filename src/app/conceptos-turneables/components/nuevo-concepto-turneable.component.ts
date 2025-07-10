dbeb1cc4ed9178ec902fc076f6f8be83f88532c0
nicolasarana <90768149+nicolasarana@users.noreply.github.com>
2025-07-10 14:55:20 -0300

    MONIT-85: Carga de nuevo concepto turneable

--- src/app/conceptos-turneables/components/nuevo-concepto-turneable.component.ts
+++ src/app/conceptos-turneables/components/nuevo-concepto-turneable.component.ts
@@ -1,6 +1,6 @@
 import { Plex } from '@andes/plex';
 import { PlexModalComponent } from '@andes/plex/src/lib/modal/modal.component';
-import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ISnomedConcept } from 'src/app/shared/ISnomedConcept';
 import { SnomedService } from 'src/app/shared/snomed.service';
@@ -11,7 +11,7 @@ import { ConceptoTruneableService } from '../services/concepto-turneable.service
     selector: 'app-concepto-turneable-nuevo',
     templateUrl: './nuevo-concepto-turneable.component.html',
 })
-export class NuevoConceptoTurneableComponent {
+export class NuevoConceptoTurneableComponent implements OnInit {
     @ViewChild('modal', { static: true }) modal: PlexModalComponent;
     @Output() agregarConceptoTurneable = new EventEmitter<IConceptoTurneable>();
     @Output() cancelarAgregarConceptoTurneable = new EventEmitter<any>();
@@ -31,6 +31,11 @@ export class NuevoConceptoTurneableComponent {
         agendaDinamica: null,
         ambito: null,
     };
+    agendaDinamica;
+    public ambitoActual;
+    public ambito;
+    public ambitos: any[];
+
     nominalizada = false;
     auditable = false;
     loading = false;
@@ -41,11 +46,78 @@ export class NuevoConceptoTurneableComponent {
         private conceptoTurneableService: ConceptoTruneableService,
     ) {
     }
+    ngOnInit() {
+        this.asignarAtributos();
+    }
 
     mostrarMensaje() {
         this.plex.info('warning', 'Ya existe un concepto turneable para el conceptId seleccionado. Para mayor información comunicarse con <b>soporteandes@neuquen.gov.ar</b>.', 'Información');
     }
 
+    asignarAtributos() {
+        if (this.nuevoConceptoTurneable.noNominalizada) {
+            this.nominalizada = false;
+        } else {
+            this.nominalizada = true;
+        }
+
+        if (this.nuevoConceptoTurneable.auditable) {
+            this.auditable = true;
+        } else {
+            this.auditable = false;
+        }
+
+        if (this.nuevoConceptoTurneable.agendaDinamica) {
+            this.agendaDinamica = true;
+        } else {
+            this.agendaDinamica = false;
+        }
+        // carga los posibles ambitos
+        this.ambitos = [
+            {
+                id: 'ambulatorio',
+                nombre: 'AMBULATORIO'
+            },
+            {
+                id: 'internacion',
+                nombre: 'INTERNACIÓN'
+            },
+            {
+                id: 'quirofano',
+                nombre: 'QUIRÓFANO'
+            },
+            {
+                id: 'guardia',
+                nombre: 'GUARDIA'
+            },
+            {
+                id: 'pre-hospitalario',
+                nombre: 'PRE-HOSPITALARIO'
+            },
+            {
+                id: 'hospital-de-dia',
+                nombre: 'HOSPITAL DE DÍA'
+            },
+            {
+                id: 'domiciliario',
+                nombre: 'DOMICILIARIO'
+            }
+        ];
+        this.ambitoActual = (this.nuevoConceptoTurneable.ambito || []).map(id => this.ambitos.find(a => a.id === id)).filter(a => !!a);
+
+    }
+
+    // convierte un array a un objeto seleccion
+    array2objectSelect(my_array) {
+        let result = [];
+        try {
+            if (!Array.isArray(my_array)) {
+                return result;
+            }
+        } catch { return result; }
+        result = my_array.map(e => ({ id: e, nombre: e.toUpperCase() }));
+        return result;
+    }
     buscarConcepto(concepto: IConceptoTurneable) {
         const existe = new Subject<boolean>();
 
@@ -58,6 +130,7 @@ export class NuevoConceptoTurneableComponent {
 
         return existe.asObservable();
     }
+
 
     buscar() {
         // Cancela la búsqueda anterior
@@ -105,6 +178,8 @@ export class NuevoConceptoTurneableComponent {
     }
 
     agregar() {
+        this.nuevoConceptoTurneable.ambito = (this.ambitoActual || []).map(a => a.id);
+
         this.nuevoConceptoTurneable.noNominalizada = !this.nominalizada;
         this.nuevoConceptoTurneable.auditable = this.auditable;
         this.agregarConceptoTurneable.emit(this.nuevoConceptoTurneable);
