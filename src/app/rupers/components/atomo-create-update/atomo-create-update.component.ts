c481c42458492f0895d450012f107f0a15f15c92
nicolasarana <90768149+nicolasarana@users.noreply.github.com>
2026-07-28 12:44:38 -0300

    Átomo: Nuevo componente genérico de texto

--- src/app/rupers/components/atomo-create-update/atomo-create-update.component.ts
+++ src/app/rupers/components/atomo-create-update/atomo-create-update.component.ts
@@ -39,9 +39,15 @@ export class RUPAtomoCreateUpdateComponent implements OnInit {
         { id: 'SelectStaticoComponent', nombre: 'Select Estatico' },
         { id: 'ObservacionesComponent', nombre: 'Observaciones' },
         { id: 'ValorNumericoComponent', nombre: 'Valor Numerico' },
-        { id: 'ChecklistComponent', nombre: 'CheckList' }
+        { id: 'ChecklistComponent', nombre: 'CheckList' },
+        { id: 'TextoSimpleComponent', nombre: 'Texto Simple' }
     ];
 
+    textoSimpleTypes = [
+        { id: 'letters', label: 'Solo letras' },
+        { id: 'numbers', label: 'Solo números' },
+        { id: 'special', label: 'Solo caracteres especiales' }
+    ];
     tipoAtomo: { id: string; nombre: string } = null;
 
     titulo = 'Nuevo átomo';
